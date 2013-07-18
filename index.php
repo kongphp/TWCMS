3851480d46263c56915c93c008bc4292c18e6c34
kongphp <kongphp@gmail.com>
2013-07-18 16:27:40 +0800

    上传测试

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 /**
- *	[TWCMS] (C)2012 TongWang Inc.
- *	$Id: index.php 475 2013-01-12 06:22:20Z wuzhaohuan $
+ *	[TWCMS] (C)2012-2013 TongWang Inc.
  */
-?>
+
+define('DEBUG', 2);	//调试模式，分三种：0 关闭调试; 1 线上调试; 2 开发调试
+define('APP_NAME', 'twcms');	//项目名称
+define('TWCMS_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');	//TWCMS目录
+define('APP_PATH', TWCMS_PATH.APP_NAME.'/');	//APP目录
+define('KONG_PATH', APP_PATH.'kongphp/');	//框架目录
+require KONG_PATH.'kongphp.php';
