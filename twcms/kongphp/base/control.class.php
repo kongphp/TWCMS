80e22a18da51318492a3d5d583c804b4f8fcd516
kongphp <kongphp@gmail.com>
2013-07-22 12:01:48 +0800

    改进控制器，不直接继承模板引擎，方便开发者可以不使用官方模板引擎，可自由使用第三方模板引擎

--- twcms/kongphp/base/control.class.php
+++ twcms/kongphp/base/control.class.php
@@ -4,6 +4,15 @@
 // +------------------------------------------------------------------------------
 
 class control extends view{
+	public function __get($var) {
+		if($var == 'view') {
+			return $this->view = new view();			
+		}elseif($var == 'db') {
+			$db = 'db_'.$_SERVER['_config']['db']['type'];
+			return $this->db = new $db($_SERVER['_config']['db']);	// 给开发者调试时使用，千万不要在控制器中操作 DB
+		}
+	}
+
 	public function message($msg, $jumpurl = '') {
 		if(R('ajax')) {
 
