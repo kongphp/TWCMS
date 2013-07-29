7f7410a4d68f32ec7b73a056fa589d5f7c5f019f
kongphp <kongphp@gmail.com>
2013-07-29 17:09:27 +0800

    增加后台APP

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *	[TWCMS] (C)2012-2013 TongWang Inc.
+ */
+
+define('DEBUG', 2);	//调试模式，分三种：0 关闭调试; 1 开启调试; 2 开发调试   注意：开启调试会暴露绝对路径和表前缀
+define('APP_NAME', 'twcms_admin');	//APP名称
+define('ADM_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');	//后台目录
+define('TWCMS_PATH', dirname(ADM_PATH).'/');	//TWCMS目录
+define('APP_PATH', TWCMS_PATH.'twcms/');	//APP目录
+define('CONTROL_PATH', ADM_PATH.'control/');	//控制器目录
+define('VIEW_PATH', ADM_PATH.'view/');	//视图目录
+define('KONG_PATH', APP_PATH.'kongphp/');	//框架目录
+require KONG_PATH.'kongphp.php';
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  */
 
 define('DEBUG', 2);	//调试模式，分三种：0 关闭调试; 1 开启调试; 2 开发调试   注意：开启调试会暴露绝对路径和表前缀
-define('APP_NAME', 'twcms');	//项目名称
+define('APP_NAME', 'twcms');	//APP名称
 define('TWCMS_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');	//TWCMS目录
 define('APP_PATH', TWCMS_PATH.APP_NAME.'/');	//APP目录
 define('KONG_PATH', APP_PATH.'kongphp/');	//框架目录
