b224f30918f18d405596ab42a8141ec3c642a990
kongphp <kongphp@gmail.com>
2013-07-30 11:48:22 +0800

    修改 字符串加密、解密函数

--- twcms/kongphp/base/base.func.php
+++ twcms/kongphp/base/base.func.php
@@ -246,32 +246,55 @@ function str_replace_once($search, $replace, $content) {
 /**
  * 字符串加密、解密函数
  * @param string $string	字符串
- * @param string $type		ENCODE为加密，DECODE为解密，可选参数，默认为ENCODE
+ * @param string $operation	ENCODE为加密，DECODE为解密，可选参数，默认为ENCODE
  * @param string $key		密钥：数字、字母、下划线
  * @param string $expiry	过期时间
  * @return string
  */
-function str_auth($string, $type = 'ENCODE', $key = '', $expiry = 0) {
-	$key_length = 4;
-	$key = empty($key) ? C('auth_key') : $key;
-	$fixedkey = md5(md5($key));
-	$egiskeys = md5(substr($fixedkey, 16, 16));
-	$runtokey = $key_length ? ($type == 'ENCODE' ? substr(md5(microtime(true)), -$key_length) : substr($string, 0, $key_length)) : '';
-	$keys = md5(substr($runtokey, 0, 16) . substr($fixedkey, 0, 16) . substr($runtokey, 16) . substr($fixedkey, 16));
-	$string = $type == 'ENCODE' ? sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$egiskeys), 0, 16) . $string : base64_decode(substr($string, $key_length));
+function str_auth($string, $operation = 'DECODE', $key = '', $expiry = 0) {
+	$ckey_length = 4;
+	$key = md5($key != '' ? $key : C('auth_key'));
+	$keya = md5(substr($key, 0, 16));
+	$keyb = md5(substr($key, 16, 16));
+	$keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length): substr(md5(microtime()), -$ckey_length)) : '';
 
-	$i = 0; $result = '';
+	$cryptkey = $keya.md5($keya.$keyc);
+	$key_length = strlen($cryptkey);
+
+	$string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0).substr(md5($string.$keyb), 0, 16).$string;
 	$string_length = strlen($string);
-	for($i = 0; $i < $string_length; $i++) {
-		$result .= chr(ord($string{$i}) ^ ord($keys{$i % 32}));
+
+	$result = '';
+	$box = range(0, 255);
+
+	$rndkey = array();
+	for($i = 0; $i <= 255; $i++) {
+		$rndkey[$i] = ord($cryptkey[$i % $key_length]);
 	}
-	if($type == 'ENCODE') {
-		return $runtokey . str_replace('=', '', base64_encode($result));
-	}else{
-		if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$egiskeys), 0, 16)) {
+
+	for($j = $i = 0; $i < 256; $i++) {
+		$j = ($j + $box[$i] + $rndkey[$i]) % 256;
+		$tmp = $box[$i];
+		$box[$i] = $box[$j];
+		$box[$j] = $tmp;
+	}
+
+	for($a = $j = $i = 0; $i < $string_length; $i++) {
+		$a = ($a + 1) % 256;
+		$j = ($j + $box[$a]) % 256;
+		$tmp = $box[$a];
+		$box[$a] = $box[$j];
+		$box[$j] = $tmp;
+		$result .= chr(ord($string[$i]) ^ ($box[($box[$a] + $box[$j]) % 256]));
+	}
+
+	if($operation == 'DECODE') {
+		if((substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0) && substr($result, 10, 16) == substr(md5(substr($result, 26).$keyb), 0, 16)) {
 			return substr($result, 26);
 		}else{
 			return '';
 		}
+	}else{
+		return $keyc.str_replace('=', '', base64_encode($result));
 	}
 }
