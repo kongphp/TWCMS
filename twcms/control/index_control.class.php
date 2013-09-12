cbe75016dbce5e97f522d5a109a5401c7485ca36
kongphp <kongphp@gmail.com>
2013-09-12 13:47:38 +0800

    首页增加读取系统配置信息

--- twcms/control/index_control.class.php
+++ twcms/control/index_control.class.php
@@ -10,6 +10,10 @@ class index_control extends control{
 	public function index() {
 		// hook index_control_index_before.php
 
+		$cfg = $this->runtime->xget();
+		$cfg['titles'] = $cfg['webname'].' - '.$cfg['seo_title'];
+		$this->assign('tw', $cfg);
+
 		$this->display('index.htm');
 	}
 
