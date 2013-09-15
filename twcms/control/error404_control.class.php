330cc081e73e44cfffaefc24dd5fad0c5e0c079d
kongphp <kongphp@gmail.com>
2013-09-15 18:51:07 +0800

    修改错误404控制器

--- twcms/control/error404_control.class.php
+++ twcms/control/error404_control.class.php
@@ -7,9 +7,24 @@
 defined('TWCMS_PATH') or exit;
 
 class error404_control extends control{
+	public $_cfg = array();	// 全站参数
+	public $_var = array();	// 404页参数
+
 	public function index() {
 		// hook error404_control_index_before.php
 
-		$this->display();
+		header('HTTP/1.1 404 Not Found');
+		header("status: 404 Not Found");
+
+		$this->_cfg = $this->runtime->xget();
+		$this->_cfg['titles'] = '404 Not Found';
+		$this->_var['topcid'] = -1;
+
+		$this->assign('tw', $this->_cfg);
+		$this->assign('_var', $this->_var);
+
+		// hook error404_control_index_after.php
+
+		$this->display('404.htm');
 	}
 }
