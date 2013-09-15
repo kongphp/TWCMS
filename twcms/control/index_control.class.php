3479b4992de95d6e29f27da796771c90ab2fcc04
kongphp <kongphp@gmail.com>
2013-09-15 16:38:05 +0800

    改进首页控制器

--- twcms/control/index_control.class.php
+++ twcms/control/index_control.class.php
@@ -7,12 +7,14 @@
 defined('TWCMS_PATH') or exit;
 
 class index_control extends control{
+	public $_cfg = array();	// 全站设置参数
+
 	public function index() {
 		// hook index_control_index_before.php
 
-		$cfg = $this->runtime->xget();
-		$cfg['titles'] = $cfg['webname'].(empty($cfg['seo_title']) ? '' : ' - '.$cfg['seo_title']);
-		$this->assign('tw', $cfg);
+		$this->_cfg = $this->runtime->xget();
+		$this->_cfg['titles'] = $this->_cfg['webname'].(empty($this->_cfg['seo_title']) ? '' : ' - '.$this->_cfg['seo_title']);
+		$this->assign('tw', $this->_cfg);
 
 		$this->display('index.htm');
 	}
