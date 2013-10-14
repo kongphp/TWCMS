0e5b4254dcd7730e072f27c6597b7e23e88b32e5
kongphp <kongphp@gmail.com>
2013-10-14 15:12:43 +0800

    增加 分类展示模块

---
+++ twcms/block/kp_block_category.lib.php
@@ -0,0 +1,47 @@
+<?php
+defined('KONG_PATH') || exit;
+
+/**
+ * 分类展示模块
+ * @param string type 显示类型    同级(sibling)、子级(child)、父级(parent)、顶级(top)
+ * @param int mid 模型ID (默认自动识别)
+ * @return array
+ */
+function kp_block_category($conf) {
+	global $run;
+
+	// hook kp_block_category_before.php
+
+	$cid = _int($_GET, 'cid');
+	$mid = isset($conf['mid']) ? intval($conf['mid']) : (isset($run->_var['mid']) ? $run->_var['mid'] : 2);
+	$type = isset($conf['type']) && in_array($conf['type'], array('sibling', 'child', 'parent', 'top')) ? $conf['type'] : 'sibling';
+
+	$cate_arr = $run->category->get_category_db();
+
+	switch($type) {
+		case 'sibling':
+			$upid = isset($cate_arr[$cid]) ? $cate_arr[$cid]['upid'] : 0;
+			break;
+		case 'child':
+			$upid = $cid;
+			break;
+		case 'parent':
+			$upid = isset($cate_arr[$cid]) ? $cate_arr[$cid]['upid'] : 0;
+			$upid = isset($cate_arr[$upid]) ? $cate_arr[$upid]['upid'] : $upid;
+			break;
+		case 'top':
+			$upid = 0;
+	}
+
+	foreach($cate_arr as $k => &$v) {
+		if($v['upid'] != $upid || $v['mid'] != $mid) {
+			unset($cate_arr[$k]);
+		}else{
+			$v['url'] = 'index.php?cate--cid-'.$v['cid'].C('url_suffix');
+		}
+	}
+
+	// hook kp_block_category_after.php
+
+	return $cate_arr;
+}
