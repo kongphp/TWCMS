0ae32ea282b3ea43c354634db5c124c7218471ba
kongphp <kongphp@gmail.com>
2013-10-25 14:18:15 +0800

    实现清除缓存功能

--- admin/control/tool_control.class.php
+++ admin/control/tool_control.class.php
@@ -9,7 +9,12 @@ defined('TWCMS_PATH') or exit;
 class tool_control extends admin_control {
 	// 清除缓存
 	public function index() {
-		// hook admin_tool_control_index_after.php
+		if(!empty($_POST)) {
+			!empty($_POST['dbcache']) && $this->runtime->truncate();
+			!empty($_POST['filecache']) && $this->un_filecache();
+			E(0, '清除完成！');
+		}
+
 		$this->display();
 	}
 
@@ -17,6 +22,19 @@ class tool_control extends admin_control {
 	public function rebuild() {
 		// hook admin_tool_control_rebuild_after.php
 		$this->display();
+	}
+
+	// 删除文件缓存
+	private function un_filecache() {
+		try{ unlink(RUNTIME_PATH.'_runtime.php'); }catch(Exception $e) {}
+		$tpmdir = array('_control', '_model', '_view');
+		foreach($tpmdir as $dir) {
+			_rmdir(RUNTIME_PATH.APP_NAME.$dir);
+		}
+		foreach($tpmdir as $dir) {
+			_rmdir(RUNTIME_PATH.F_APP_NAME.$dir);
+		}
+		return TRUE;
 	}
 
 	// hook admin_tool_control_after.php
