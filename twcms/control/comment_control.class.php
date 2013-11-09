fe42341ddb2a9a02e381a7b10e091618975e20d2
kongphp <kongphp@gmail.com>
2013-11-09 20:04:48 +0800

    增加评论页控制器

---
+++ twcms/control/comment_control.class.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * (C)2012-2013 twcms.cn TongWang Inc.
+ * Author: wuzhaohuan <kongphp@gmail.com>
+ */
+
+defined('TWCMS_PATH') or exit;
+
+class comment_control extends control{
+	public $_cfg = array();	// 全站参数
+	public $_var = array();	// 内容页参数
+
+	public function index() {
+		// hook comment_control_index_before.php
+
+		$_GET['cid'] = (int)R('cid');
+		$_GET['id'] = (int)R('id');
+		$this->_var = $this->category->get_cache($_GET['cid']);
+		empty($this->_var) && core::error404();
+
+		$this->_cfg = $this->runtime->xget();
+
+		// 初始模型表名
+		$this->cms_content->table = 'cms_'.$this->_var['table'];
+
+		// 读取内容
+		$_show = $this->cms_content->read($_GET['id']);
+		empty($_show) && core::error404();
+
+		// SEO 相关
+		$this->_cfg['titles'] = $_show['title'];
+		$this->_cfg['seo_keywords'] = empty($_show['seo_keywords']) ? $_show['title'] : $_show['seo_keywords'];
+		$this->_cfg['seo_description'] = empty($_show['seo_description']) ? $_show['intro']: $_show['seo_description'];
+
+		$this->assign('tw', $this->_cfg);
+		$this->assign('tw_var', $this->_var);
+
+		$GLOBALS['run'] = &$this;
+
+		// hook comment_control_index_after.php
+
+		$_ENV['_theme'] = &$this->_cfg['theme'];
+		$this->display('comment.htm');
+	}
+
+	// hook comment_control_after.php
+}
