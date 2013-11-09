c9910ada849f1037bf822182d564cb9bd7c6fea7
kongphp <kongphp@gmail.com>
2013-11-09 21:31:04 +0800

    增加内容评论模型

---
+++ twcms/model/cms_content_comment_model.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * (C)2012-2013 twcms.cn TongWang Inc.
+ * Author: wuzhaohuan <kongphp@gmail.com>
+ */
+
+defined('TWCMS_PATH') or exit;
+
+class cms_content_comment extends model {
+	function __construct() {
+		$this->table = '';			// 表名 (可以是 cms_article_comment cms_product_comment cms_photo_comment 等)
+		$this->pri = array('id', 'commentid');	// 主键
+		$this->maxid = 'commentid';		// 自增字段
+	}
+
+	// 获取评论列表
+	public function list_arr($where, $orderway, $start, $limit, $total) {
+		// 优化大数据量翻页
+		if($start > 1000 && $total > 2000 && $start > $total/2) {
+			$orderway = -$orderway;
+			$newstart = $total-$start-$limit;
+			if($newstart < 0) {
+				$limit += $newstart;
+				$newstart = 0;
+			}
+			$list_arr = $this->find_fetch($where, array('commentid' => $orderway), $newstart, $limit);
+			return array_reverse($list_arr, TRUE);
+		}else{
+			return $this->find_fetch($where, array('commentid' => $orderway), $start, $limit);
+		}
+	}
+}
