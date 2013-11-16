f282a21c64d2c077798da637c40fb0ee7b89b174
kongphp <kongphp@gmail.com>
2013-11-16 16:11:46 +0800

    增加前台搜索控制器

---
+++ twcms/control/search_control.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * (C)2012-2013 twcms.cn TongWang Inc.
+ * Author: wuzhaohuan <kongphp@gmail.com>
+ */
+
+defined('TWCMS_PATH') or exit;
+
+class search_control extends control{
+	public function index() {
+
+	}
+}
