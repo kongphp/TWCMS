8bb608483464bae90579c0a094ca03e79c2bcb0a
kongphp <kongphp@gmail.com>
2013-11-17 20:38:57 +0800

    实现 搜索页 控制器

--- twcms/control/search_control.class.php
+++ twcms/control/search_control.class.php
@@ -7,7 +7,28 @@
 defined('TWCMS_PATH') or exit;
 
 class search_control extends control{
-	public function index() {
+	public $_cfg = array();	// 全站参数
+	public $_var = array();	// 搜索页参数
 
+	public function index() {
+		// hook search_control_index_before.php
+
+		$keyword = R('keyword');
+
+		$this->_cfg = $this->runtime->xget();
+		$this->_cfg['titles'] = $keyword;
+		$this->_var['topcid'] = -1;
+
+		$this->assign('tw', $this->_cfg);
+		$this->assign('tw_var', $this->_var);
+		$this->assign('keyword', $keyword);
+
+		$GLOBALS['run'] = &$this;
+		$GLOBALS['keyword'] = &$keyword;
+
+		// hook search_control_index_after.php
+
+		$_ENV['_theme'] = &$this->_cfg['theme'];
+		$this->display('search.htm');
 	}
 }
