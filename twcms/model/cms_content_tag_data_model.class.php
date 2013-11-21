e7b8cbbb14ef68081e1df700e33be28b3cae2dce
kongphp <kongphp@gmail.com>
2013-11-21 14:35:19 +0800

    增加 获取标签列表 方法

--- twcms/model/cms_content_tag_data_model.class.php
+++ twcms/model/cms_content_tag_data_model.class.php
@@ -11,4 +11,21 @@ class cms_content_tag_data extends model {
 		$this->table = '';					// 表名 (可以是 cms_article_tag_data cms_product_tag_data cms_photo_tag_data 等)
 		$this->pri = array('tagid', 'id');	// 主键
 	}
+
+	// 获取标签列表
+	public function list_arr($tagid, $orderway, $start, $limit, $total) {
+		// 优化大数据量翻页
+		if($start > 1000 && $total > 2000 && $start > $total/2) {
+			$orderway = -$orderway;
+			$newstart = $total-$start-$limit;
+			if($newstart < 0) {
+				$limit += $newstart;
+				$newstart = 0;
+			}
+			$list_arr = $this->find_fetch(array('tagid' => $tagid), array('id' => $orderway), $newstart, $limit);
+			return array_reverse($list_arr, TRUE);
+		}else{
+			return $this->find_fetch(array('tagid' => $tagid), array('id' => $orderway), $start, $limit);
+		}
+	}
 }
