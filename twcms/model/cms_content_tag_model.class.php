d535df840d38ed0ff6b803b49fe750d7139ca92d
kongphp <kongphp@gmail.com>
2013-11-22 16:32:50 +0800

    增加 获取标签列表 方法

--- twcms/model/cms_content_tag_model.class.php
+++ twcms/model/cms_content_tag_model.class.php
@@ -17,4 +17,21 @@ class cms_content_tag extends model {
 	public function format(&$v, $mid) {
 		$v['url'] = 'index.php?tag--mid-'.$mid.'-name-'.urlencode($v['name']).C('url_suffix');
 	}
+
+	// 获取标签列表
+	public function list_arr($orderway, $start, $limit, $total) {
+		// 优化大数据量翻页
+		if($start > 1000 && $total > 2000 && $start > $total/2) {
+			$orderway = -$orderway;
+			$newstart = $total-$start-$limit;
+			if($newstart < 0) {
+				$limit += $newstart;
+				$newstart = 0;
+			}
+			$list_arr = $this->find_fetch(array(), array('count' => $orderway), $newstart, $limit);
+			return array_reverse($list_arr, TRUE);
+		}else{
+			return $this->find_fetch(array(), array('count' => $orderway), $start, $limit);
+		}
+	}
 }
