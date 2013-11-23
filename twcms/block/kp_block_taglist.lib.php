f73a52e8d8b3e43d07ff8772e23110205810d2cf
kongphp <kongphp@gmail.com>
2013-11-24 00:24:02 +0800

    修改全局标签模块为局部模块

--- twcms/block/kp_block_taglist.lib.php
+++ twcms/block/kp_block_taglist.lib.php
@@ -2,26 +2,26 @@
 defined('KONG_PATH') || exit;
 
 /**
- * 标签展示页模块
- * @param int maxcount 显示最大标签数
+ * 标签列表模块
+ * @param int limit 显示几条标签
  * @return array
  */
-function kp_block_global_tagall($conf) {
+function kp_block_taglist($conf) {
 	global $run;
 
-	// hook kp_block_global_tagall_before.php
+	// hook kp_block_taglist_before.php
 
-	$maxcount = isset($conf['maxcount']) ? (int)$conf['maxcount'] : 1000;
+	$limit = isset($conf['limit']) ? (int)$conf['limit'] : 10;
 	$mid = max(2, (int)R('mid'));
 	$table = isset($run->_cfg['table_arr'][$mid]) ? $run->_cfg['table_arr'][$mid] : 'article';
 
 	$run->cms_content_tag->table = 'cms_'.$table.'_tag';
-	$list_arr = $run->cms_content_tag->find_fetch(array(), array('count'=>-1), 0, $maxcount);
+	$list_arr = $run->cms_content_tag->find_fetch(array(), array('count'=>-1), 0, $limit);
 	foreach($list_arr as &$v) {
 		$run->cms_content_tag->format($v, $mid);
 	}
 
-	// hook kp_block_global_tagall_after.php
+	// hook kp_block_taglist_after.php
 
 	return array('list'=>$list_arr);
 }
