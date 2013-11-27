60e61c2b4049b0b82dcc66210c2b4703e1378a50
kongphp <kongphp@gmail.com>
2013-11-27 11:27:02 +0800

    增加后台附件控制器

---
+++ admin/control/attach_control.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * (C)2012-2013 twcms.cn TongWang Inc.
+ * Author: wuzhaohuan <kongphp@gmail.com>
+ */
+
+defined('TWCMS_PATH') or exit;
+
+class attach_control extends admin_control {
+
+	// hook admin_attach_control_after.php
+}
