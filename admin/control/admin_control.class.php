b324dd066f064434c836701703fec2bba873dd17
kongphp <kongphp@gmail.com>
2013-12-03 18:28:50 +0800

    附件设置改为上传设置（砍掉 远程抓取设置）

--- admin/control/admin_control.class.php
+++ admin/control/admin_control.class.php
@@ -165,7 +165,7 @@ class admin_control extends control {
 				'setting-index'=>array('name'=>'基本设置', 'p'=>'setting'),
 				'setting-seo'=>array('name'=>'SEO设置', 'p'=>'setting'),
 				'setting-link'=>array('name'=>'链接设置', 'p'=>'setting'),
-				'setting-attach'=>array('name'=>'附件设置', 'p'=>'setting'),
+				'setting-attach'=>array('name'=>'上传设置', 'p'=>'setting'),
 
 				'category-index'=>array('name'=>'分类管理', 'p'=>'category'),
 				'navigate-index'=>array('name'=>'导航管理', 'p'=>'category'),
