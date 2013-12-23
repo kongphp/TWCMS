14624a16aa6930443a56032fb90f57613e39ec45
kongphp <kongphp@gmail.com>
2013-12-23 17:31:20 +0800

    增加 cms_content_flag 模型

---
+++ twcms/model/cms_content_flag_model.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * (C)2012-2013 twcms.cn TongWang Inc.
+ * Author: wuzhaohuan <kongphp@gmail.com>
+ */
+
+defined('TWCMS_PATH') or exit;
+
+class cms_content_flag extends model {
+	function __construct() {
+		$this->table = '';					// 表名 (可以是 cms_article_flag cms_product_flag cms_photo_flag 等)
+		$this->pri = array('flag', 'id');	// 主键
+	}
+}
