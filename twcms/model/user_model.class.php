b7ef38f72b1c4cc817b21fcf78630a92abb0646a
kongphp <kongphp@gmail.com>
2014-05-17 19:43:23 +0800

    修正开启缓存后 current 数组指针不对问题。

--- twcms/model/user_model.class.php
+++ twcms/model/user_model.class.php
@@ -16,7 +16,7 @@ class user extends model {
 	// 根据用户名获取用户数据
 	public function get_user_by_username($username) {
 		$data = $this->find_fetch(array('username'=>$username), array(), 0, 1);
-		return $data ? current($data) : array();
+		return $data ? array_pop($data) : array();
 	}
 
 	// 检查用户名是否合格
